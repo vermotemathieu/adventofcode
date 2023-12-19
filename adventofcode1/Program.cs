8dd5f3d5924b01a6e2ae4f787faea8c91fdc11eb
Mathieu Vermote <mathieu@outlook.be>
2023-12-19 13:44:54 +0100

    1

---
+++ adventofcode1/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+var lines = File.ReadAllLines("puzzleinput.txt");
+
+var sum = 0;
+
+foreach (var line in lines)
+{
+    var digits = line.Where(Char.IsDigit).ToArray();
+
+    var first = digits[0];
+    var last = digits[digits.Count() - 1];
+
+    string concat = String.Concat(first, last);
+
+    sum += Int32.Parse(concat);
+
+}
+
+Console.WriteLine("SUM: " + sum);
