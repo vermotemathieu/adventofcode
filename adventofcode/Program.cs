d62de5f01a74a02779b723bd4a8028c14492b7c3
Mathieu Vermote <mathieu@outlook.be>
2023-12-19 17:04:47 +0100

    day1

---
+++ adventofcode/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using adventofcode;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("------------------------------------");
+
+Console.WriteLine("Output Day1 - Part 1: {0}", Day1.Part1());
+Console.WriteLine("Output Day1 - Part 2: {0}", Day1.Part2());
+
