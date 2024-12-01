41c17c43d1fa6f49f2c9cc2caa36a200e8dbffb6
Mathieu Vermote <mathieu@outlook.be>
2024-12-01 13:17:58 +0100

    add 2024

---
+++ adventofcode/2024/Day1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adventofcode._2024
+{
+    internal class Day1
+    {
+    }
+}
